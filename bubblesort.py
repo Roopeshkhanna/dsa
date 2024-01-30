9c1ad053e8bea000f1e5a460668bdc20159d4ba6
Roopeshkhanna <98201415+Roopeshkhanna@users.noreply.github.com>
2024-01-30 21:46:52 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,14 +1,12 @@
 lis=[2,1,5,7,2,5,8,9,33]
-
-for i in range(len(lis)-1,-1,-1):
-    swap=0
-    for j in range(0,i):
-     
-      if(lis[j]>lis[j+1]):
-            lis[j+1],lis[j]=lis[j],lis[j+1]
-            swap+=1
-    if(not swap):
-        break
+swap=0
+for i in range(len(lis)):
+    for j in range(1,len(lis)-i):
+        if(lis[j]<lis[j-1]):
+             swap+=1
+             lis[j-1],lis[j]=lis[j],lis[j-1]
+    if(swap==0):
+      break
 print(lis)
-#time complexity is o(n^2)
-#in best case o(n)
+//Time complexity o(n^2) 
+//space complexity o(1)
