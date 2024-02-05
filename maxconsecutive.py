287397a33ce739b58dd0531e311818abaa3ccd36
Roopeshkhanna <roopeshkhannab@gmail.com>
2024-02-05 18:42:01 +0530

    maxi

---
+++ maxconsecutive.py
@@ -0,0 +1,11 @@
+nums = [1,1,0,1,1,1]
+count=0
+maxi=0
+for i in range(len(nums)):
+    if(nums[i]==1):
+        count+=1
+        maxi = max(maxi, count)
+        
+    else:
+        count=0
+print(maxi)
