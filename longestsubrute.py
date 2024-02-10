c5d79f839b69d6b5ffe4b2de8c3e1b16f0e2cd77
Roopeshkhanna <roopeshkhannab@gmail.com>
2024-02-10 12:02:49 +0530

    subarray

--- longestsubrute.py
+++ longestsubrute.py
@@ -1,13 +1,14 @@
-a=[1,2,3,1,1,1,1,2,1]
+a=[1,2,3,1,1,1,2,1]
 leng=0
 s=3
 for i in range(len(a)):
+    sum=0
     for j in range(i,len(a)):
-        sum=0
-        for k in range(i,j+1):
-            sum+=a[k]
+       
+       
+        sum+=a[j]
         if(sum==3):
             leng=max(leng,j-i+1)
 print(leng)
-#T.C O(n^3)
+#T.C O(n^2)
 #S.C O(1)
